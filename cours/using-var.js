7997b85ae0af0ea48debead424ac4305b7d43a86
Yassine Rassy <47438695+yassineDevox@users.noreply.github.com>
2021-09-30 13:34:58 +0200

    using var done

---
+++ cours/using-var.js
@@ -0,0 +1,9 @@
+//prompt a and b 
+var a = parseInt(prompt("Enter the value of a : "))
+var b = parseInt(prompt("Entrer the value of b : "))
+
+//treatment
+var d = a/b;
+
+if(b==0) alert("Error division on Zero 😥")
+else alert("d = "+d)
